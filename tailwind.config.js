7df584992c3edddddcf4d32d3e3af7b942a7d310
vargha1 <vargha.rahavi@gmail.com>
2024-10-13 13:24:51 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./main.js", "./model1.html", "./model.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
