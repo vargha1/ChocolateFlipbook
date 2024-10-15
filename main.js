5005d39c6b9c6a3979bb26e82869aad31a21d3f9
vargha1 <vargha.rahavi@gmail.com>
2024-10-15 14:21:13 +0330

    VR

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 import '/style.css'
 
 $('#magazine').turn({ gradients: true, acceleration: true });
+
+if (window.innerWidth > 1280) {
+    for (let i = 3; i < 7; i++) {
+        document.getElementById(`image${i}`).remove()
+    }
+    document.getElementById("image1").setAttribute("src", "images/fff2.png")
+    document.getElementById("image2").setAttribute("src", "images/fff22.png")
+}
