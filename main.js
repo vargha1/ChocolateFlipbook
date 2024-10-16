fd4f6768b1510a2a32b4f286433ab8cf0102bd55
vargha1 <vargha.rahavi@gmail.com>
2024-10-16 16:58:11 +0330

    VR

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ import '/style.css'
 $('#magazine').turn({ gradients: true, acceleration: true });
 
 if (window.innerWidth > 1280) {
-    for (let i = 3; i < 7; i++) {
-        document.getElementById(`image${i}`).remove()
-    }
-    document.getElementById("image1").setAttribute("src", "images/fff2.png")
-    document.getElementById("image2").setAttribute("src", "images/fff22.png")
+    document.getElementById("modelLink").className = "absolute top-12 w-[250px] h-[200px]"
+    document.getElementById("image1").setAttribute("src", "images/1ps.png")
+    document.getElementById("image2").setAttribute("src", "images/3ps.png")
+    document.getElementById("image3").setAttribute("src", "images/2ps.png")
+    document.getElementById("image4").setAttribute("src", "images/4ps.png")
+    document.getElementById("image5").setAttribute("src", "images/5ps.png")
+    document.getElementById("image6").setAttribute("src", "images/6ps.png")
 }
